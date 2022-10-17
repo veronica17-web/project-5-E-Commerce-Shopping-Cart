003e287b89feac2a6cefcba4b416014242a80757
Jitendrayadav7999 <jitendra7999yadav@outlook.com>
2022-10-17 11:58:09 +0530

    done

--- src/model/cartModel.js
+++ src/model/cartModel.js
@@ -6,7 +6,6 @@ const cartSchema = new mongoose.Schema(
     userId: { type: ObjectId, ref: "User", required: true, unique: true },
     items: [
       {
-        _id: false,
         productId: { type: ObjectId, ref: "Product", required: true },
         quantity: { type: Number, required: true, min: 1 },
       },
